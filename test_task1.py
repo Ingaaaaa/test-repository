c65280d90cf0bc6d8553a2f9db900ce5e3a25f9d
Ingaaaaa <49069404+Inga@users.noreply.github.com>
2019-08-16 04:12:53 -0600

    add task1

---
+++ test_task1.py
@@ -0,0 +1,11 @@
+import pytest
+from selenium import webdriver
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    request.addfinalizer(wd.quit)
+    return wd
+
+def test_task1(driver):
+    driver.get("https://www.onliner.by")
